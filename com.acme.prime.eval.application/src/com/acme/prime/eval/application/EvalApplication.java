15a97d6ff04baff62215c86278ae0bfa857d94dd
Krzysztof Gołębiowski <krzysiek.golebiowski@gmail.com>
2017-02-19 22:10:43 +0100

    Implemented EvalApplication

--- com.acme.prime.eval.application/src/com/acme/prime/eval/application/EvalApplication.java
+++ com.acme.prime.eval.application/src/com/acme/prime/eval/application/EvalApplication.java
@@ -1,22 +1,29 @@
 package com.acme.prime.eval.application;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
-import osgi.enroute.configurer.api.RequireConfigurerExtender;
-import osgi.enroute.google.angular.capabilities.RequireAngularWebResource;
-import osgi.enroute.rest.api.REST;
-import osgi.enroute.twitter.bootstrap.capabilities.RequireBootstrapWebResource;
-import osgi.enroute.webserver.capabilities.RequireWebServerExtender;
+import com.acme.prime.eval.api.Eval;
 
-@RequireAngularWebResource(resource={"angular.js","angular-resource.js", "angular-route.js"}, priority=1000)
-@RequireBootstrapWebResource(resource="css/bootstrap.css")
-@RequireWebServerExtender
-@RequireConfigurerExtender
-@Component(name="com.acme.prime.eval")
-public class EvalApplication implements REST {
+import osgi.enroute.debug.api.Debug;
 
-	public String getUpper(String string) {
-		return string.toUpperCase();
+@Component(
+		service=EvalApplication.class, 
+		property = { 
+			Debug.COMMAND_SCOPE + "=eval",
+			Debug.COMMAND_FUNCTION + "=eval" 
+		},
+		name="com.acme.prime.eval"
+	)
+public class EvalApplication {
+	private Eval eval;
+
+	public double eval(String m) throws Exception {
+		return eval.eval(m);
 	}
-
+	
+	@Reference
+	void setEval( Eval eval) {
+		this.eval = eval;
+	}
 }
