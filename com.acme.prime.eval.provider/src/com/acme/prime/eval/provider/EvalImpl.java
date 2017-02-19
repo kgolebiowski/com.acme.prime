f77dc87a6162441a080cda56abcd0d2dc9a74509
Krzysztof Gołębiowski <krzysiek.golebiowski@gmail.com>
2017-02-19 17:45:20 +0100

    Provided simple implementation for EvalImpl

--- com.acme.prime.eval.provider/src/com/acme/prime/eval/provider/EvalImpl.java
+++ com.acme.prime.eval.provider/src/com/acme/prime/eval/provider/EvalImpl.java
@@ -1,11 +1,31 @@
 package com.acme.prime.eval.provider;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.osgi.service.component.annotations.Component;
+
+import com.acme.prime.eval.api.Eval;
 
 /**
  * 
  */
 @Component(name = "com.acme.prime.eval")
-public class EvalImpl {
-
+public class EvalImpl implements Eval {
+	Pattern EXPR = Pattern.compile( "\\s*(?<left>\\d+)\\s*(?<op>\\+|-)\\s*(?<right>\\d+)\\s*");
+	
+	@Override
+	public double eval(String expression) throws Exception {
+		Matcher m = EXPR.matcher(expression);
+		if ( !m.matches())
+			throw new IllegalArgumentException("Invalid expression " + expression);
+		
+		double left = Double.valueOf( m.group("left"));
+		double right = Double.valueOf( m.group("right"));
+		switch( m.group("op")) {
+		case "+": return left + right;
+		case "-": return left - right;
+		}
+		return Double.NaN;
+	}
 }
