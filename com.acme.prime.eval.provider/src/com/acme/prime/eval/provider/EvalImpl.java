367a93eb013d175165b4c171b8df48be5be9cbcf
Krzysztof Gołębiowski <krzysiek.golebiowski@gmail.com>
2017-02-19 20:25:01 +0100

    Replaces custom implementation of EvalImpl with the one provided by Parsii

--- com.acme.prime.eval.provider/src/com/acme/prime/eval/provider/EvalImpl.java
+++ com.acme.prime.eval.provider/src/com/acme/prime/eval/provider/EvalImpl.java
@@ -1,13 +1,11 @@
 package com.acme.prime.eval.provider;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 import org.osgi.service.component.annotations.Component;
 
 import com.acme.prime.eval.api.Eval;
 
 import osgi.enroute.debug.api.Debug;
+import parsii.eval.Parser;
 
 /**
  * 
@@ -20,26 +18,9 @@ import osgi.enroute.debug.api.Debug;
 		}
 )
 public class EvalImpl implements Eval {
-	Pattern EXPR = Pattern.compile( "\\s*(?<left>\\d+)\\s*(?<op>\\+|-|\\*|/)\\s*(?<right>\\d+)\\s*");
 	
 	@Override
 	public double eval(String expression) throws Exception {
-		Matcher m = EXPR.matcher(expression);
-		if ( !m.matches())
-			throw new IllegalArgumentException("Invalid expression " + expression);
-		
-		double left = Double.valueOf( m.group("left"));
-		double right = Double.valueOf( m.group("right"));
-		switch (m.group("op")) {
-			case "+":
-				return left + right;
-			case "-":
-				return left - right;
-			case "*":
-				return left * right;
-			case "/":
-				return left / right;
-		}
-		return Double.NaN;
+		return Parser.parse(expression).evaluate();
 	}
 }
